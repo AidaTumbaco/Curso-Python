50ac4f143f86a5e811d69ea8fac53864b9628e95
AidaTumbaco <aptumbac@espol.edu.ec>
2025-01-09 17:02:38 -0500

    Update 3.3_lista_campeones_del_mundo.py

--- Ejercicios/3.3_lista_campeones_del_mundo.py
+++ Ejercicios/3.3_lista_campeones_del_mundo.py
@@ -24,6 +24,25 @@ suplentes = ["Marchena", "Torres", "Valdes", "Fabregas", "Mata", "Arbeloa",
    “suplentes” y eliminar a Pedrito de la lista de titulares y meterle en 
    la de suplentes.
 """
+El template de este ejercicio lo puedes encontrar en el siguiente enlace: GitHub
+
+Dadas las siguientes variables que representan a los jugadores de la selección Española de fútbol que jugo el mundial del 2010:
+
+portero = "Casillas" 
+defensas_titulares = ["Ramos", "Pique", "Puyol", "Capdevila"] 
+centrocampistas = ["Xabi Alonso", "Busquets", "Xavi Hernandez", "Iniesta"] 
+delanteros = ["Pedrito", "Villa"] 
+suplentes = ["Marchena", "Torres", "Valdes", 
+             "Fabregas", "Mata", "Arbeloa", 
+             "Llorente", "Javi Martinez", "Silva", 
+             "Reina", "Navas"]
+Se pide:
+
+Contar con cuantos delanteros jugo España la final del mundial. (lista delanteros)
+Crear una nueva lista llamada “titulares” que contenga a los jugadores titulares de la selección Española.
+En el minuto 60’ del partido se realiza un cambio: entra Navas por Pedrito. En la lista de “titulares” hay que meter
+a Navas que está en la lista de “suplentes” y eliminar a Pedrito de la lista de titulares y meterle en la de suplentes.
+Crear una lista llamada “selección” que contenga a todos los jugadores de la selección Española.
 
 
 """
