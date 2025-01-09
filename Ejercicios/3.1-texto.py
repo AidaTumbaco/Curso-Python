44834a6599ca3fedca0ade87ac2521844df00ea1
AidaTumbaco <aptumbac@espol.edu.ec>
2025-01-09 17:00:01 -0500

    Update 3.1-texto.py

--- Ejercicios/3.1-texto.py
+++ Ejercicios/3.1-texto.py
@@ -1,14 +1,10 @@
-"""
-Crea un texto de al menos 10 caracteres
-"""
+# Crear un texto de al menos 10 caracteres
+texto = "Programación"
 
+# Imprimir la letra que está en el índice 7
+letra_indice_7 = texto[7]
+print(f"La letra en el índice 7 es: {letra_indice_7}")
 
-"""
-Imprime la letra que está en el índice 7
-"""
-
-
-"""
-Imprime las letras que están en el rango desde la posición 3 a la 8
-"""
-
+# Imprimir las letras que están en el rango desde la posición 3 a la 8
+letras_rango = texto[3:8]
+print(f"Las letras desde la posición 3 a la 8 son: {letras_rango}")
