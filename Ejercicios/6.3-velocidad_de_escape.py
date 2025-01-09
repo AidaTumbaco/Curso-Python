66bf52a4c6d1b0e1bca4773dd1a988d0944bcc41
AidaTumbaco <aptumbac@espol.edu.ec>
2025-01-09 17:45:46 -0500

    Update 6.3-velocidad_de_escape.py

--- Ejercicios/6.3-velocidad_de_escape.py
+++ Ejercicios/6.3-velocidad_de_escape.py
@@ -1,3 +1,18 @@
 """
 Calculadora de la velocidad de escape.
 """
+Nos han contratado en la Agencia Espacial Internacional, tienen intención de poner en órbita unos satélites, pensando en el futuro cuando lleguemos a Marte y a la Luna también querrán lanzar satélites desde ahí.
+
+Por lo que quieren una calculadora que introduciendo la masa de un planeta y su radio nos devuelva la «velocidad de escape» de cada planeta en concreto.
+
+Pistas:
+
+Nos documentamos https://es.wikipedia.org/wiki/Velocidad_de_escape
+Buscamos la fórmula que nos piden crear en la calculadora
+Localizamos las variables de la fórmula (G es Constante de gravitación universal, M es Masa del planeta y r es Radio del planeta)
+Programamos la calculadora
+Una vez que lo tengamos, probaremos con los datos de los planetas que nos han pedido (Buscamos los datos en Internet): Tierra, Marte y Luna
+
+Es importante comprobar los datos que introduzca el usuario: si han metido números y no otra cosa, que no sean vacíos, etc.
+
+Ejemplo:
