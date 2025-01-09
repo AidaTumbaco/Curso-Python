14943c776d8cf0842bd2c8f0a02bfe43b503d86c
AidaTumbaco <aptumbac@espol.edu.ec>
2025-01-09 16:38:24 -0500

    Update 2.1-tienda_ropa.py

--- Ejercicios/2.1-tienda_ropa.py
+++ Ejercicios/2.1-tienda_ropa.py
@@ -1,8 +1,20 @@
-"""
-Aplicar la plantilla: "{ropa} cuesta {numero} euros" a dos prendas de ropa
-"""
+# Crear variable formato
+formato = "{ropa} cuesta {numero} euros"
 
+# Crear dos variables con prendas de ropa
+ropa1 = "Camisa"
+ropa2 = "Pantalón"
 
-"""
-Aplicar la plantilla: "Su carro de la compra tiene: <formato_aplicado1> y <formato_aplicado2>"
-"""
+# Crear dos variables con un número escrito
+numero1 = "20"
+numero2 = "40"
+
+# Crear variables formato_aplicado aplicando el formato
+formato_aplicado1 = formato.format(ropa=ropa1, numero=numero1)
+formato_aplicado2 = formato.format(ropa=ropa2, numero=numero2)
+
+# Crear una última variable que junte las otras dos
+carro_compra = f"Su carro de la compra tiene: {formato_aplicado1} y {formato_aplicado2}"
+
+# Imprimir la última variable
+print(carro_compra)
